415df0cec82ab82bdd19fd16037abcb70d5f7d3b
Javier Barreiro Portela <jbarreiro.23@gmail.com>
2025-01-23 17:08:24 +0100

    Doing last part of 4.0 exercises

---
+++ Lists_02.py
@@ -0,0 +1,18 @@
+﻿# 4.7 List multiple of 3
+threes = []
+
+for number in range(3,31):
+    if number % 3 == 0:
+        threes.append(number)
+
+print(threes)
+
+# 4.8 Cubes
+cubes_no_comprehension = []
+for number in range(1,11):
+    cubes_no_comprehension.append(number**3)
+    print( number**3 )
+
+# 4.9 Cube comprehension
+cubes = [ num ** 3 for num in range(1,11)]
+print(cubes)
